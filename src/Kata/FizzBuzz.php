f288c5aa22107970ca325bf9e26fb26760d501e2
Mario Spallazzi <mario@MacBook-Pro-di-Mario.local>
2022-10-25 23:24:48 +0200

    feat: WP

--- src/Kata/FizzBuzz.php
+++ src/Kata/FizzBuzz.php
@@ -4,8 +4,16 @@ namespace Kata;
 
 class FizzBuzz
 {
-    public function handle(): bool
+    public function retrieveArrayOfNumbersAndFizz(): array
     {
-        return true;
+        for ($number = 1; $number <= 100; $number++) {
+            if ($number === 3 || $number === 6 || $number === 9) {
+                $arrayNumberAndFizz[] = 'Fizz';
+            } else {
+                $arrayNumberAndFizz[] = $number;
+            }
+        }
+
+        return $arrayNumberAndFizz;
     }
 }
--- tests/Kata/FizzBuzzTest.php
+++ tests/Kata/FizzBuzzTest.php
@@ -3,24 +3,68 @@
 namespace Kata;
 
 use PHPUnit\Framework\TestCase;
-use Kata\FizzBuzz;
 
 class FizzBuzzTest extends TestCase
 {
-    private $fizzBuzz;
+    private FizzBuzz $fizzBuzz;
 
     protected function setUp(): void
     {
         $this->fizzBuzz = new FizzBuzz();
     }
 
-    public function testShallPass(): void
+    public function testReturnNumber1(): void
     {
-        $this->assertEquals(1, 1);
+        $this->assertContains(1, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
     }
 
-    public function testHandleReturnTrue(): void
+    public function testReturnNumber1AndNumber2(): void
     {
-        $this->assertEquals(true, $this->fizzBuzz->handle());
+        $this->assertContains(1, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
+        $this->assertContains(2, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
+    }
+
+    public function testReturnNumber1Number2Number4(): void
+    {
+        $this->assertContains(1, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
+        $this->assertContains(2, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
+        $this->assertContains(4, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
+    }
+
+    public function testNotReturnNumberGreaterThan100(): void
+    {
+        $this->assertNotContains(101, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
+        $this->assertNotContains(1007, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
+        $this->assertNotContains(1222, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
+        $this->assertNotContains(111, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
+    }
+
+    public function testNotReturnNumberLessThan0(): void
+    {
+        $this->assertNotContains(-1, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
+        $this->assertNotContains(-100, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
+        $this->assertNotContains(-12, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
+        $this->assertNotContains(-67, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
+    }
+
+    public function testReturnStringFizzForNumber3(): void
+    {
+        $ActualArray = $this->fizzBuzz->retrieveArrayOfNumbersAndFizz();
+
+        $this->assertEquals('Fizz', $ActualArray[2]);
+    }
+
+    public function testReturnStringFizzForNumber6(): void
+    {
+        $ActualArray = $this->fizzBuzz->retrieveArrayOfNumbersAndFizz();
+
+        $this->assertEquals('Fizz', $ActualArray[5]);
+    }
+
+    public function testReturnStringFizzForNumber9(): void
+    {
+        $ActualArray = $this->fizzBuzz->retrieveArrayOfNumbersAndFizz();
+
+        $this->assertEquals('Fizz', $ActualArray[8]);
     }
 }
