5fa3034e08b9406b430a38172dd13f0f65857378
Mario Spallazzi <mario@MacBook-Pro-di-Mario.local>
2022-10-25 23:36:10 +0200

    feat: WP

--- src/Kata/FizzBuzz.php
+++ src/Kata/FizzBuzz.php
@@ -4,16 +4,27 @@ namespace Kata;
 
 class FizzBuzz
 {
-    public function retrieveArrayOfNumbersAndFizz(): array
+    public function retrieveArrayOfNumbersOrFizzOrBuzz(): array
     {
         for ($number = 1; $number <= 100; $number++) {
-            if ($number === 3 || $number === 6 || $number === 9) {
-                $arrayNumberAndFizz[] = 'Fizz';
-            } else {
-                $arrayNumberAndFizz[] = $number;
+            if (is_int($number / 3) && is_int($number / 5)) {
+                $arrayNumberOrFizzOrBuzz[] = 'FizzBuzz';
+                continue;
             }
+
+            if (is_int($number / 3)) {
+                $arrayNumberOrFizzOrBuzz[] = 'Fizz';
+                continue;
+            }
+
+            if (is_int($number / 5)) {
+                $arrayNumberOrFizzOrBuzz[] = 'Buzz';
+                continue;
+            }
+
+            $arrayNumberOrFizzOrBuzz[] = $number;
         }
 
-        return $arrayNumberAndFizz;
+        return $arrayNumberOrFizzOrBuzz;
     }
 }
--- tests/Kata/FizzBuzzTest.php
+++ tests/Kata/FizzBuzzTest.php
@@ -15,56 +15,91 @@ class FizzBuzzTest extends TestCase
 
     public function testReturnNumber1(): void
     {
-        $this->assertContains(1, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
+        $this->assertContains(1, $this->fizzBuzz->retrieveArrayOfNumbersOrFizzOrBuzz());
     }
 
     public function testReturnNumber1AndNumber2(): void
     {
-        $this->assertContains(1, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
-        $this->assertContains(2, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
+        $this->assertContains(1, $this->fizzBuzz->retrieveArrayOfNumbersOrFizzOrBuzz());
+        $this->assertContains(2, $this->fizzBuzz->retrieveArrayOfNumbersOrFizzOrBuzz());
     }
 
     public function testReturnNumber1Number2Number4(): void
     {
-        $this->assertContains(1, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
-        $this->assertContains(2, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
-        $this->assertContains(4, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
+        $this->assertContains(1, $this->fizzBuzz->retrieveArrayOfNumbersOrFizzOrBuzz());
+        $this->assertContains(2, $this->fizzBuzz->retrieveArrayOfNumbersOrFizzOrBuzz());
+        $this->assertContains(4, $this->fizzBuzz->retrieveArrayOfNumbersOrFizzOrBuzz());
     }
 
     public function testNotReturnNumberGreaterThan100(): void
     {
-        $this->assertNotContains(101, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
-        $this->assertNotContains(1007, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
-        $this->assertNotContains(1222, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
-        $this->assertNotContains(111, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
+        $this->assertNotContains(101, $this->fizzBuzz->retrieveArrayOfNumbersOrFizzOrBuzz());
+        $this->assertNotContains(1007, $this->fizzBuzz->retrieveArrayOfNumbersOrFizzOrBuzz());
+        $this->assertNotContains(1222, $this->fizzBuzz->retrieveArrayOfNumbersOrFizzOrBuzz());
+        $this->assertNotContains(111, $this->fizzBuzz->retrieveArrayOfNumbersOrFizzOrBuzz());
     }
 
     public function testNotReturnNumberLessThan0(): void
     {
-        $this->assertNotContains(-1, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
-        $this->assertNotContains(-100, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
-        $this->assertNotContains(-12, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
-        $this->assertNotContains(-67, $this->fizzBuzz->retrieveArrayOfNumbersAndFizz());
+        $this->assertNotContains(-1, $this->fizzBuzz->retrieveArrayOfNumbersOrFizzOrBuzz());
+        $this->assertNotContains(-100, $this->fizzBuzz->retrieveArrayOfNumbersOrFizzOrBuzz());
+        $this->assertNotContains(-12, $this->fizzBuzz->retrieveArrayOfNumbersOrFizzOrBuzz());
+        $this->assertNotContains(-67, $this->fizzBuzz->retrieveArrayOfNumbersOrFizzOrBuzz());
     }
 
     public function testReturnStringFizzForNumber3(): void
     {
-        $ActualArray = $this->fizzBuzz->retrieveArrayOfNumbersAndFizz();
+        $ActualArray = $this->fizzBuzz->retrieveArrayOfNumbersOrFizzOrBuzz();
 
         $this->assertEquals('Fizz', $ActualArray[2]);
     }
 
     public function testReturnStringFizzForNumber6(): void
     {
-        $ActualArray = $this->fizzBuzz->retrieveArrayOfNumbersAndFizz();
+        $ActualArray = $this->fizzBuzz->retrieveArrayOfNumbersOrFizzOrBuzz();
 
         $this->assertEquals('Fizz', $ActualArray[5]);
     }
 
     public function testReturnStringFizzForNumber9(): void
     {
-        $ActualArray = $this->fizzBuzz->retrieveArrayOfNumbersAndFizz();
+        $ActualArray = $this->fizzBuzz->retrieveArrayOfNumbersOrFizzOrBuzz();
 
         $this->assertEquals('Fizz', $ActualArray[8]);
+    }
+
+    public function testReturnStringFizzForNumber99(): void
+    {
+        $ActualArray = $this->fizzBuzz->retrieveArrayOfNumbersOrFizzOrBuzz();
+
+        $this->assertEquals('Fizz', $ActualArray[98]);
+    }
+
+    public function testReturnStringBuzzForNumber5(): void
+    {
+        $ActualArray = $this->fizzBuzz->retrieveArrayOfNumbersOrFizzOrBuzz();
+
+        $this->assertEquals('Buzz', $ActualArray[4]);
+    }
+
+    public function testReturnStringBuzzForNumber10(): void
+    {
+        $ActualArray = $this->fizzBuzz->retrieveArrayOfNumbersOrFizzOrBuzz();
+
+        $this->assertEquals('Buzz', $ActualArray[9]);
+    }
+
+    public function testReturnStringBuzzForNumber100(): void
+    {
+        $ActualArray = $this->fizzBuzz->retrieveArrayOfNumbersOrFizzOrBuzz();
+
+        $this->assertEquals('Buzz', $ActualArray[99]);
+    }
+
+    public function testReturnStringFizzBuzzForNumber30(): void
+    {
+        $ActualArray = $this->fizzBuzz->retrieveArrayOfNumbersOrFizzOrBuzz();
+
+        $this->assertEquals('FizzBuzz', $ActualArray[29]);
     }
 }
